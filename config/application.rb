9b81cd1fdefcb31fcdbb8eeac0f5b64fcb05c95f
Nandha <nandhagr8@gmail.com>
2018-12-24 12:40:54 +0530

    [Nandha] Fix application rb

--- config/application.rb
+++ config/application.rb
@@ -22,16 +22,16 @@ module FueloApi
     config.load_defaults 5.2
 
     config.api_only = true
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource(
           '*',
           headers: :any,
-          methods: [:get, :patch, :put, :delete, :post, :options]
+          methods: [:get, :patch, :put, :delete, :post, :options, :show]
         )
       end
     end
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
   end
 end
